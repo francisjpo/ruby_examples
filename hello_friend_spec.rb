06f5493558a0a075e5b3f1a21c5ae9f9b5a86b60
Francisco P <github@franciscopinto.me>
2015-06-20 13:04:31 +0100

    First commit

---
+++ hello_friend_spec.rb
@@ -0,0 +1,14 @@
+# # Hello, friend!
+#
+
+require "friend"
+
+describe Friend do
+  it "says hello" do
+    Friend.new.greeting.should == "Hello!"
+  end
+
+  it "says hello to someone" do
+    Friend.new.greeting("Bob").should == "Hello, Bob!"
+  end
+end
